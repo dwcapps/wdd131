007241e7f4f89a849f9474ed9a8bd1f5af27a315
dwcapps <dannycapps0419@gmail.com>
2025-02-15 00:25:43 -0700

    Completed W06 Build a Blog Part 2

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,42 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
-	}
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+		}
 ]
+
+const mainContent = document.getElementById("maincontent");
+
+for (let i = 0; i < articles.length; i++) {
+	let article = articles[i];
+	const newArticle = document.createElement("section");
+	newArticle.classList.add("book-listing");
+	newArticle.setAttribute("id", article.id);
+	const HTMLTemplate = `
+            <article class="article-body">
+                <h2 class="title">${article.title}</h2>
+                <img class = "cover" src="${article.imgSrc}" alt="${article.imgAlt}">
+                <p class="description">${article.description}</p>
+            </article>
+            <aside class="info-box">
+                <h3 class="date">${article.date}</h3>
+                <p class="ages">${article.ages}</p>
+                <p class="genre">${article.genre}</p>
+                <p class="stars">${article.stars}</p>
+            </aside>
+		`;
+	newArticle.innerHTML = HTMLTemplate;
+	mainContent.appendChild(newArticle);
+}
